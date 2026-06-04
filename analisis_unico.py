af9e77ee358ba240d12b7e2310949d63d9784d54
taty2346-a11y <taty2346@gmail.com>
2026-06-04 12:38:22 +0200

    analisis_unico.py

--- analisis_unico.py
+++ analisis_unico.py
@@ -310,5 +310,4 @@ if archivo_carga:
             st.dataframe(df_final, use_container_width=True)
             
             csv = df_final.to_csv(index=False).encode('utf-8')
-            st.download_button(
-            label="💾 Guardar Reporte Comparativo (Excel)", data=excel_data, file_name="comparativa_consolidada.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+            st.download_button(label="💾 Guardar Reporte Comparativo (Excel)", data=excel_data, file_name="comparativa_consolidada.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
