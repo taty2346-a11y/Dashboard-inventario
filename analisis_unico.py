2ae6dbe653f1cad56f9addeca64228b1cb67ecbb
taty2346-a11y <taty2346@gmail.com>
2026-06-08 08:04:19 +0200

    Update analisis_unico.py

--- analisis_unico.py
+++ analisis_unico.py
@@ -186,15 +186,15 @@ if archivo_carga:
             else:
                 pct_reubicados, pct_tallas, pct_puro = 0.0, 0.0, 0.0
 
-# Mostrar Métricas
-        st.subheader("📌 Resumen Ejecutivo")
-        c1, c2, c3, c4, c5 = st.columns(5)
-        c1.metric("Correcto", len(df[df['Categoria'] == "Correcto"]))
-        c2.metric("Found", len(df[df['Categoria'] == "Found"]))
-        c3.metric("Lost", len(df[df['Categoria'] == "Lost"]))
-        c4.metric("Reubicado", total_reubicados)
-        c5.metric("Cambio Talla", total_tallas)
-        
+                # Mostrar Métricas
+            st.subheader("📌 Resumen Ejecutivo")
+            c1, c2, c3, c4, c5 = st.columns(5)
+            c1.metric("Correcto", len(df[df['Categoria'] == "Correcto"]))
+            c2.metric("Found", len(df[df['Categoria'] == "Found"]))
+            c3.metric("Lost", len(df[df['Categoria'] == "Lost"]))
+            c4.metric("Reubicado", total_reubicados)
+            c5.metric("Cambio Talla", total_tallas)
+         
         st.dataframe(df, use_container_width=True)
             
             # --- SECCIÓN 2: GRÁFICOS CON LOS COLORES AJUSTADOS ---
