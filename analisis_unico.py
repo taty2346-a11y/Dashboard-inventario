3c5f8d092222c835e9618c63dc6009265df22f62
taty2346-a11y <taty2346@gmail.com>
2026-06-08 07:46:45 +0200

    Update analisis_unico.py

--- analisis_unico.py
+++ analisis_unico.py
@@ -45,7 +45,6 @@ st.markdown("""
             position: static !important;
             background-color: white !important;
         }
-        
         /* Ocultar la barra lateral izquierda por completo en el PDF */
         section[data-testid="stSidebar"] {
             display: none !important;
